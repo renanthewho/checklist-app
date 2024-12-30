0a814d4bd16f06652eabb26e12e3c20c8ab4164a
Renan Almeida <44383286+renanthewho@users.noreply.github.com>
2024-12-30 17:35:36 -0300

    Inclusão do modulo home, correção do app.module e inclusão de rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { FormsModule } from '@angular/forms';
     CategoryComponent,
     TestComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
     
   ],
   imports: [
