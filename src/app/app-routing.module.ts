47a4525e448b3a241c501848de13318f15113623
Renan Almeida <44383286+renanthewho@users.noreply.github.com>
2025-01-15 21:21:14 -0300

    Inclusão de rota de categoria.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
   {path:'' , component: HomeComponent},
+  {path:'categoria' , component: CategoryComponent},
   {path:'**' , component: PageNotFoundComponent}
 ];
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   imports: [MatToolbarModule,
@@ -12,14 +13,16 @@ import {MatListModule} from '@angular/material/list';
     MatIconModule,
     MatSidenavModule,
     MatCheckboxModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule,
   ],
   exports: [MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatCheckboxModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
